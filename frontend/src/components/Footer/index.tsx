5fa7043c8151293204e53c4f0465081d11a99652
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2024-05-13 23:05:06 -0300

    Mexendo estilização componentes página login

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,36 +1,31 @@
-import { Button } from "@mui/material";
 import style from "./style.module.css";
 
 import { siteInfos } from "@/utils/siteInfos";
 import LogoComponent from "../Logo";
 
+import { BsArrowUp } from "react-icons/bs";
+import { ButtonComponent } from "../Buttons";
+
 interface IFooterComponent {
- styles?: any;
+  styles?: any;
 }
 
 export default function FooterComponent({ styles }: IFooterComponent) {
- return (
-  <footer className={style.page_footer} style={styles}>
-   <LogoComponent
-    logoColor="var(--whiteFixed)"
-    secondNameColor="var(--whiteFixed)"
-   />
-   <span>{siteInfos.copyright}</span>
-   <Button
-    className={style.footer_button}
-    onClick={() => {
-     document.body.scrollIntoView();
-    }}
-   >
-    <span
-     style={{
-      fontSize: "2rem",
-      color: "var(--brand-4)",
-     }}
-    >
-     ^
-    </span>
-   </Button>
-  </footer>
- );
+  return (
+    <footer className={style.page_footer} style={styles}>
+      <LogoComponent
+        logoColor="var(--whiteFixed)"
+        secondNameColor="var(--whiteFixed)"
+      />
+      <span>{siteInfos.copyright}</span>
+      <ButtonComponent
+        className={style.footer_button}
+        onClick={() => {
+          document.body.scrollIntoView();
+        }}
+      >
+        <BsArrowUp className={style.footer_button_svg} size={"1.25rem"} />
+      </ButtonComponent>
+    </footer>
+  );
 }
